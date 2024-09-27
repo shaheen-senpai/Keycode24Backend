2d4cfd11c13e1664e0e216d497c974abeeb017b9
shaheen-senpai <shaheen@keyvalue.systems>
2024-09-27 23:45:34 +0530

    Add LessonPlan entity and its relations to User and Subject entities

---
+++ src/core/classroom/entity/lesson-plain.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import BaseEntity from '../../../common/utils/base.entity';
+import User from 'src/core/authorization/entity/user.entity';
+import Subject from './subject.entity';
+
+@Entity()
+class LessonPlan extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  public id!: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  public name!: string | null;
+
+  @Column({ nullable: true, type: 'uuid' })
+  public createdById!: string | null;
+
+  @ManyToOne(() => User, (user) => user.createdLessonPlans)
+  public createdBy?: User;
+
+  @ManyToOne(() => Subject, (Subject) => Subject.lessonPlans)
+  public subject?: Subject;
+}
+
+export default LessonPlan;
--- src/core/classroom/entity/subject.entity.ts
+++ src/core/classroom/entity/subject.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import BaseEntity from '../../../common/utils/base.entity';
 import Assessment from './assessment.entity';
+import LessonPlan from './lesson-plain.entity';
 
 @Entity()
 class Subject extends BaseEntity {
@@ -15,6 +16,9 @@ class Subject extends BaseEntity {
 
   @OneToMany(() => Assessment, (assessment) => assessment.subject)
   public subjectAssessments?: Assessment[];
+
+  @OneToMany(() => LessonPlan, (lessonPlan) => lessonPlan.subject)
+  public lessonPlans?: LessonPlan[];
 }
 
 export default Subject;
