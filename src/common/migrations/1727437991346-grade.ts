00e3337b92421f8c982d603c7f6dc0e25860f892
akhil.r <akhil.r@keyvalue.systems>
2024-09-27 18:53:48 +0530

    migration

---
+++ src/common/migrations/1727437991346-grade.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Grade1727437991346 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.query(
+          `INSERT INTO public.grade (name, created_by_id) VALUES
+          ('Grade 1', (select id from public.user where email = 'susan@gmail.com')),
+          ('Grade 2', (select id from public.user where email = 'susan@gmail.com')),
+          ('Grade 3', (select id from public.user where email = 'susan@gmail.com')),
+          ('Grade 4', (select id from public.user where email = 'susan@gmail.com')),
+          ('Grade 5', (select id from public.user where email = 'susan@gmail.com')),
+          ('Grade 6', (select id from public.user where email = 'susan@gmail.com')),
+          ('Grade 7', (select id from public.user where email = 'susan@gmail.com')),
+          ('Grade 8', (select id from public.user where email = 'susan@gmail.com'))`,
+        );
+        await queryRunner.query(
+          `INSERT INTO public.subject (name, file_url) VALUES
+          ('Maths', 'https://dcgrwjnegntwzuvtcybn.supabase.co/storage/v1/object/public/assets/11-Redblack.pdf'),
+          ('Science', 'https://dcgrwjnegntwzuvtcybn.supabase.co/storage/v1/object/public/assets/11-Redblack.pdf'),
+          ('Englis', 'https://dcgrwjnegntwzuvtcybn.supabase.co/storage/v1/object/public/assets/11-Redblack.pdf');`,
+        );
+        await queryRunner.query(
+          `INSERT INTO public.subject_grade (subject_id, grade_id) SELECT (select id from subject where name = 'Maths'), id FROM public.grade;`,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
